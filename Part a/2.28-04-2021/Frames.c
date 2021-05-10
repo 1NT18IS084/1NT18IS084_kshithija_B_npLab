2aba4313c02464d24ebd9d83607222efdc176821
1NT18IS084 <82608467+1NT18IS084@users.noreply.github.com>
2021-05-10 21:02:28 +0530

    Create Frames.c

---
+++ Part a/2.28-04-2021/Frames.c
@@ -0,0 +1,37 @@
+/*Design a C program in which sender module shoukd count the number of bytes in the frame and receiver module should display each frame received*/
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("enter no of frames\n");
+	scanf("%d",&n);
+	long int fr[n];
+	printf("enter the frame\n");
+		
+	int i;
+			
+	for(i=0;i<n;i++)
+		
+	scanf("%ld",&fr[i]);
+			
+	int j=0;
+	
+int x,y;
+int b=0;
+
+	while(j<n){
+		x=fr[j];
+
+		while(x>0){
+			x=x/10;
+			b++;
+		}
+		printf("size of frame=%d\n",b);
+		b=0;
+		j++;
+	}
+	printf("frames received are:\n");
+	for(i=0;i<n;i++)
+	printf("%ld\n",fr[i]);
+	return 0;
+}
